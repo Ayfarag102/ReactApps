67cb68b2ff2ccc2f34ec93a2c1fe1ddc75a19641
Aly Farag <afarag@Alys-MacBook-Pro.local>
2020-08-16 03:49:32 -0400

    deleted unneeded files

--- instagram-clone/instagram-clone/src/App.js
+++ instagram-clone/instagram-clone/src/App.js
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello MXS Coding and Productions</h1>
+      <h6>
+        Let's build an Instagram Clone with React{" "}
+        <span role="img" aria-label="">
+          🚀
+        </span>
+      </h6>
     </div>
   );
 }
